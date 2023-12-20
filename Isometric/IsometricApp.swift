6475051dd8ab1ceefb6befb749a8e2c6e5419aac
PrathameshBhuravane <bhuravaneprathamesh@gmail.com>
2023-12-20 12:11:15 +0530

    Initial Commit

---
+++ Isometric/IsometricApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsometricApp.swift
+//  Isometric
+//
+//  Created by Prathamesh on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct IsometricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
